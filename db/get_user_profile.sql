e41cd585dd75e8bad1b563c221117b47ee22e901
brian1959 <brian@solutionation.net>
2018-10-02 16:29:30 -0600

    message table built

--- db/get_user_profile.sql
+++ db/get_user_profile.sql
@@ -1,7 +1,3 @@
-SELECT lu.*, lp.*, ls.*
-FROM limber_User as lu
- JOIN limber_profile as lp ON 
- lp.user_id = lu.user_id 
- JOIN limber_settings as ls ON 
- lu.user_id = ls.user_id
-where lu.user_id = $1;
+SELECT *
+FROM limber_profile 
+where user_id = $1;
