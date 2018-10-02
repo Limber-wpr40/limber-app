bd7fbc36e30b4f6c21b1afda5a1572d51c549208
brian1959 <brian@solutionation.net>
2018-10-02 15:04:45 -0600

    server file initile build

---
+++ server/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const massive = require("massive");
+const session = require("express-session");
+const axios = require("axios");
+
+const app = express();
+
+const {
+    SERVER_PORT,
+    CONNECTION_STRING,
+  } = process.env;
+
+massive(CONNECTION_STRING).then(db => {
+    app.set("db", db);
+  });
+  app.use(bodyParser.json());
+  
+  app.use(express.static(`${__dirname}/../build`));
+
+
+  app.listen(SERVER_PORT, () => {
+    console.log(`Server evesdropping on port ${SERVER_PORT}.`);
+  });
