f42a624b36fb650aafef9a847edf2ad27b6870fc
brian1959 <brian@solutionation.net>
2018-10-02 18:46:38 -0600

    delete user added and server.js updated

--- server/server.js
+++ server/server.js
@@ -9,24 +9,27 @@ const uo = require("./user_controller");
 
 const app = express();
 
-const {
-    SERVER_PORT,
-    CONNECTION_STRING,
-  } = process.env;
-
-  app.get("/api/user", uo.getUserData);
-  app.get("/api/profile", uo.getUserProfile);
-  app.get("api/matches", uo.getMatches);
-  app.get("api/possiblematch", uo.getPossibleMatches)
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
 
 massive(CONNECTION_STRING).then(db => {
-    app.set("db", db);
-  });
-  app.use(bodyParser.json());
-  
-  app.use(express.static(`${__dirname}/../build`));
+  app.set("db", db);
+});
+app.use(bodyParser.json());
 
+app.use(express.static(`${__dirname}/../build`));
 
-  app.listen(SERVER_PORT, () => {
-    console.log(`Server evesdropping on port ${SERVER_PORT}.`);
-  });
+app.get("/api/user", uo.getUserData);
+app.get("/api/profile", uo.getUserProfile);
+app.get("/api/matches", uo.getMatches);
+app.get("/api/possiblematch", uo.getPossibleMatches);
+
+app.post("/api/likes", uo.addLike);
+
+app.put("/api/user/:id", uo.updateSettings);
+app.put("/api/profile/:id", uo.updateProfile);
+
+app.delete("/api/user/:id", uo.deleteUser);
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server evesdropping on port ${SERVER_PORT}.`);
+});
