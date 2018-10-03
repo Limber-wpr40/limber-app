5c3c93dd678d17bd992fd7e2ef829eda3820b9ce
brian1959 <brian@solutionation.net>
2018-10-03 10:36:00 -0600

    all endpoints are working!

--- server/message_controller.js
+++ server/message_controller.js
@@ -1,33 +1,26 @@
 module.exports = {
-
-getMessages:(req,res)=>{
-    const db = req.app.get('db');
-    let {sender_id, recv_id} = req.query;
-    console.log(`My sender and receiver are: ${sender_id} and ${recv_id}`)
+  getMessages: (req, res) => {
+    const db = req.app.get("db");
+    let { sender_id, recv_id } = req.query;
     db.get_message_thread(sender_id, recv_id)
-        .then(messages => res.status(200).send(messages))
-        .catch(err => {
-            res.status(500).send({ errorMessage: "Oops! Something went wrong. Our engineers have been informed!" });
-            console.log(err)
-        });
-},
+      .then(messages => res.status(200).send(messages))
+      .catch(err => {
+        res
+          .status(500)
+          .send({
+            errorMessage:
+              "Oops! Something went wrong. Our engineers have been informed!"
+          });
+        console.log(err);
+      });
+  },
 
-    addMessage: async (req, res, next) => {
-        const db = req.app.get('db');
-        const {create_date, sender_id, recv_id, msg_body} = req.body;
-        console.log(`the date created is ${create_date}`)
-         await db.add_message(create_date,sender_id,recv_id,msg_body)
-        let updatedMessage = await  db.get_message_thread(sender_id, recv_id)
-            res.status(200).send(updatedMessage)
-           
-            
-
-    },
-
-
-
-
-
-
-
-}
+  addMessage: async (req, res, next) => {
+    const db = req.app.get("db");
+    const { create_date, sender_id, recv_id, msg_body } = req.body;
+    console.log(`the date created is ${create_date}`);
+    await db.add_message(create_date, sender_id, recv_id, msg_body);
+    let updatedMessage = await db.get_message_thread(sender_id, recv_id);
+    res.status(200).send(updatedMessage);
+  }
+};
