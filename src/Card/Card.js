769f63018440d4d0ff2e370cd7ff83c5d29e1dd3
Lillian Wiley <lillianwiley92@gmail.com>
2018-10-10 16:31:13 -0600

    testing done

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -4,16 +4,17 @@ import { render } from 'react-dom';
 import MotionStack from 'react-motion-stack';
 import 'react-motion-stack/build/motion-stack.css';
 import './Card.css';
+import punks from '../jestutilities/jest-testing';
 
 
 export default class Card extends Component {
-    constructor(){
+    constructor() {
         super();
 
         this.state = {
             potMatches: [],
-            super_like:false,
-            match_id:0
+            super_like: false,
+            match_id: 0
 
         }
     }
@@ -21,44 +22,48 @@ export default class Card extends Component {
     onBeforeSwipe = (swipe, direction, state) => {
         console.log('direction', direction);
         console.log('state', state.pressedId);
-        
+
+        punks.handleDirectionChange(direction);
+        punks.handleState(state);
+
         swipe();
-        if(direction === 'right'){
-                       this.setState({super_like:true, match_id:state.pressedId})
+        if (direction === 'right') {
+            this.setState({ super_like: true, match_id: state.pressedId })
             console.log(this.state.match_id, this.state.super_like)
             axios.post('/api/likes', this.state.match_id, this.state.super_like)
-          console.log('LIKE!')
-        }else{
-          console.log('NOPE!')
+            console.log('LIKE!')
+        } else {
+            console.log('NOPE!')
         }
-      }
+    }
 
     onSwipeEnd = ({ data }) => {
         console.log("data", data);
+        punks.handleOnSwipeEnd(data)
     };
 
     componentDidMount() {
         axios.get('/api/possiblematches')
-        .then(res => {
-            res.data.forEach((potMatch) => {
-                let {user_id, user_image, user_name, user_age, user_job, user_school, user_distance} = potMatch
-                potMatch.id = user_id
-                potMatch.element = (
-                    <div className="user-card">
-                        <img src={`../images/${user_image}`} alt=""/>
-                        <h1>{user_name}</h1>
-                        <h2>{user_age}</h2>
-                        <p>{user_job}</p>
-                        <p>{user_school}</p>
-                        <p>{user_distance}</p>
-                    </div>
-                )
+            .then(res => {
+                res.data.forEach((potMatch) => {
+                    let { user_id, user_image, user_name, user_age, user_job, user_school, user_distance } = potMatch
+                    potMatch.id = user_id
+                    potMatch.element = (
+                        <div className="user-card">
+                            <img src={`../images/${user_image}`} alt="" />
+                            <h1>{user_name}</h1>
+                            <h2>{user_age}</h2>
+                            <p>{user_job}</p>
+                            <p>{user_school}</p>
+                            <p>{user_distance}</p>
+                        </div>
+                    )
+                })
+                this.setState({
+                    potMatches: res.data
+                })
+                console.log(res.data)
             })
-            this.setState({
-                potMatches: res.data
-            })
-            console.log(res.data)
-        })
 
 
     }
